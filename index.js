257e5a2d209699cac4de39333357717114edfb4b
Max Kapshtyk <max@4bis.nl>
2019-07-17 18:06:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,49 @@ module.exports=function(options){
       });
    };
 
-   return {
-      sendMail: sendMail
+
+    /**
+     * sendMail wrapper to send an email with password reset link
+     * The options object would have the parameters link, appName, user
+     * @param {Object} options
+     * @returns {Promise}
+     */
+    var sendPasswordResetEmail = function({ link, appName, user }) {
+        //return sendMail({
+        //    templateName: 'passwordResetEmail',
+        //    link,
+        //    appName,
+        //    user
+        //});
+
+        sendEmail(user.get('email'), appName + ' : Password Reset Email',
+            '<h2 style="color: lightslategray">Password Reset Email</h2>' +
+            '<div>' + user.get('email') + ', to reset your password follow this link <a href="' + link + '">'+ link +'</a></div>');
+    };
+
+    /**
+     * sendMail wrapper to send an email with an account verification link
+     * The options object would have the parameters link, appName, user
+     * @param {Object} options
+     * @returns {Promise}
+     */
+    var sendVerificationEmail = function({ link, appName, user }) {
+        //return sendMail({
+        //    templateName: 'verificationEmail',
+        //    link,
+        //    appName,
+        //    user
+        //});
+
+        sendEmail(user.get('email'), appName + ' : Email Verification',
+            '<h2 style="color: forestgreen">Email Verification</h2>' +
+            '<div>' + user.get('email') + ', to verify your email follow this link <a href="' + link + '">'+ link +'</a></div>')
+    };
+
+
+    return {
+        sendMail: sendMail,
+        sendPasswordResetEmail: sendPasswordResetEmail,
+        sendVerificationEmail: sendVerificationEmail
    }
 };
